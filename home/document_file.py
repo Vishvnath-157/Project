4a9bbcb3c8aa533e40786a9ef27e639ba305c908
Vishvnath-157 <85423341+Vishvnath-157@users.noreply.github.com>
2023-03-30 14:38:16 +0530

    Update document_file.py
    security reasons

--- home/document_file.py
+++ home/document_file.py
@@ -2,14 +2,14 @@ import os
 
 
 log_path = os.getcwd()
-client_id = '1ZFEE6GY5Z-100'
-secret_key = 'WI9JMHCADW'
+client_id = '*****-100'
+secret_key = '*****'
 redirect_url = 'http://127.0.0.1:5000/login'
-username = 'XV18626'
-password ='Vishv@15'
-pin1 = '9'
-pin2 = '1'
-pin3 = '4'
-pin4 = '4'
+username = '*****'
+password ='*****'
+pin1 = '*'
+pin2 = '*'
+pin3 = '*'
+pin4 = '*'
 response_type = 'code'
 grant_type = 'authorization_code'
